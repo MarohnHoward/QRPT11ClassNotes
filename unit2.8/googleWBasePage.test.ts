37055a6eeb46d9357416af3f3ef4a4dcd1a5e5c3
Marohn Howard <mars.howard@devmounta.in>
2023-05-31 20:32:18 -0600

    finished 2.8

--- unit2.8/googleWBasePage.test.ts
+++ unit2.8/googleWBasePage.test.ts
@@ -1,10 +1,23 @@
 import {Google} from './googlePageWBase';
 const google = new Google();
-
+const fs = require('fs')
 test('do a search', async () => {
     await google.navigate();
-    await google.search('Chicago Cubs');
+    await google.search("Philidelphia Philly's");
     let text = await google.getResults();
-    expect(text).toContain('Chicago Cubs');
+    expect(text).toContain("Philidelphia Philly's");
+
+    fs.writeFile(`${__dirname}/test.txt`, text, (e) => {
+        if (e) console.log(e);
+        else console.log('Save Succesful');
+    } )
+
+    fs.writeFile(`${__dirname}/google.png`, 
+    await google.driver.takeScreenshot(), "base64", 
+    (e) =>{
+        if (e) console.log(e);
+        else console.log("a pictures says a thousand words.")
+    })
+
     await google.driver.quit()
 });
